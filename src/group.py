44c89429654daaa325d6d18e0b5379116d6207dd
honins <honins@qq.com>
2021-08-09 23:47:32 +0800

    add: 添加素材；更新团练代码

--- src/group.py
+++ src/group.py
@@ -1,16 +1,40 @@
 import pyautogui
+import time
 
 # 判定目标截图在系统上的位置
-location = pyautogui.locateAllOnScreen(image='../static/down-min.png')
-# 输出坐标
 
-for i in location:
-    print(i)
+# location = []
+#
+# location1 = [pyautogui.locateAllOnScreen(image='../static/down-min.png')]
+# location1 = [pyautogui.locateAllOnScreen(image='../static/down-min.png')]
+# location2 = [pyautogui.locateAllOnScreen(image='../static/down-min.png')]
+#
+# location.append(location1)
+# location.append(location2)
 
-    # 利用center()函数获取目标图像在系统中的中心坐标位置
-    x, y = pyautogui.center(i)
-    print('center()', x, y)
+count = 0
 
-    # 对识别出的目标图像进行点击
-    # 参数x,y代表坐标位置，clicks代表点击次数,button可以设置为左键或者右键
-    pyautogui.click(x=x, y=y, clicks=1, button='left')
+while True:
+
+    count = count + 1
+
+    print(count)
+
+    location = pyautogui.locateOnScreen(image='../static/up.png')
+
+    # 输出坐标
+    print(location)
+
+    if location:
+        # 利用center()函数获取目标图像在系统中的中心坐标位置
+        x, y = pyautogui.center(location)
+        print('center()', x, y)
+
+        # 对识别出的目标图像进行点击
+        # 参数x,y代表坐标位置，clicks代表点击次数,button可以设置为左键或者右键
+        pyautogui.click(x=x, y=y, clicks=1, button='left')
+
+        print("do click!")
+
+    else:
+        print("None!")
