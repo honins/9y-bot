c88d38e53076d9ee7d94361525cf2dfc26646120
honins <honins@qq.com>
2021-08-18 22:38:12 +0800

    add: 更新README

--- src/group.py
+++ src/group.py
@@ -36,11 +36,11 @@ while True:
     # location = pyautogui.locateOnScreen(image='../static/right.png', region=(740, 750, 450, 50), confidence=0.9)
 
     # 半屏团练
-    # location = pyautogui.locateOnScreen(image='../static/right.png', region=(260, 750, 700, 50), confidence=0.9)
+    location = pyautogui.locateOnScreen(image='../static/right.png', region=(260, 750, 700, 50), confidence=0.9)
     # location = pyautogui.locateOnScreen(image='../static/tmp.png', region=(260, 750, 700, 50), confidence=0.9)
 
     # 测试名字
-    location = pyautogui.locateOnScreen(image='../static/tmp.png', confidence=0.9)
+    # location = pyautogui.locateOnScreen(image='../static/tmp.png', confidence=0.9)
 
     # 输出坐标
     print(location)
